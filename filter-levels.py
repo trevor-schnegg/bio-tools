10a8c443c0e68264b0985a389aa299bdcc18dced
trevor_schnegg <trevor.schneggenburger@gmail.com>
2023-05-24 17:00:43 -0400

    added option to dry run the filter

--- filter-levels.py
+++ filter-levels.py
@@ -12,6 +12,13 @@ def main():
     parser = argparse.ArgumentParser(
         description="Outputs a fasta file with the input taxon levels excluded")
     parser.add_argument(
+        "-d",
+        "--dry-run",
+        dest="dry_run",
+        action="store_true",
+        help="Option to not actually output sequences only to log what the size change would be"
+    )
+    parser.add_argument(
         "accession2taxid",
         help="accession2taxid of reference file")
     parser.add_argument(
@@ -54,10 +61,22 @@ def main():
 
     # Read through reference fasta
     reference = SeqIO.parse(args.reference_fasta, 'fasta')
-    for record in reference:
-        tax_node = taxonomy.node(accession2taxid[record.id])
-        if tax_node.rank not in exclude_taxon_levels:
-            SeqIO.write(record, sys.stdout, "fasta")
+
+    if args.dry_run:
+        total_sequence_len = 0
+        filtered_sequence_len = 0
+        for record in reference:
+            total_sequence_len += len(record.seq)
+            tax_node = taxonomy.node(accession2taxid[record.id])
+            if tax_node.rank not in exclude_taxon_levels:
+                filtered_sequence_len += len(record.seq)
+        logging.info(f"total sequence length was: {total_sequence_len}")
+        logging.info(f"filtered sequence length was {filtered_sequence_len}")
+    else:
+        for record in reference:
+            tax_node = taxonomy.node(accession2taxid[record.id])
+            if tax_node.rank not in exclude_taxon_levels:
+                SeqIO.write(record, sys.stdout, "fasta")
 
 
 if __name__ == '__main__':
