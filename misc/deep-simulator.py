3351c2bedf06791c327fc49b85c2e528f5b17d20
trevor-schnegg <trevor.schneggenburger@gmail.com>
2023-06-14 13:09:34 -0400

    Changed the deepsimulator bash script to use default parameters

--- misc/deep-simulator.py
+++ misc/deep-simulator.py
@@ -16,19 +16,20 @@ def create_deepsim_bash_script(args_tuple):
 
     fasta_file = fasta_file_path.split("/")[-1]
     output_path = os.path.join(output_dir, fasta_file)
-    num_reads = num_reads if num_reads is not None else round((longest_record_length / 220) * 0.008)
+    num_reads = num_reads if num_reads is not None else round(
+        (longest_record_length / 220) * 0.008)
 
     if num_reads > 0:
         # If the longest record is None, then there is only one record in the fasta file. We can just run Deep Simulator
         # on the original file
         if longest_record is None:
-            print(f"{deepsim_binary} -i {fasta_file_path} -o {output_path} -n {num_reads} -c 20 -e 1.25 -S {random.randint(0, 1000000000)}")
+            print(f"{deepsim_binary} -i {fasta_file_path} -o {output_path} -n {num_reads} -c 20 -S {random.randint(0, 1000000000000)}")
         else:
             temp_file = os.path.join(output_dir, fasta_file + ".tmp")
             with open(temp_file, "w") as f:
                 SeqIO.write(longest_record, f, 'fasta')
 
-            print(f"{deepsim_binary} -i {temp_file} -o {output_path} -n {num_reads} -c 20 -e 1.25 -S {random.randint(0, 1000000000)}")
+            print(f"{deepsim_binary} -i {temp_file} -o {output_path} -n {num_reads} -c 20 -S {random.randint(0, 1000000000000)}")
             print(f"rm {temp_file}")
 
 
