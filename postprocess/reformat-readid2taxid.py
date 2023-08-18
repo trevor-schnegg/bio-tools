c31bbb47ff7b7d55f2265d8bd457599d702e527d
trevor <trevor.schneggenburger@gmail.com>
2023-08-18 17:53:56 -0400

    Added kASA parsing to reformat-readid2taxid.py

--- postprocess/reformat-readid2taxid.py
+++ postprocess/reformat-readid2taxid.py
@@ -3,20 +3,34 @@ import logging
 import sys
 
 
-def get_readid2taxid(filename):
+def get_seqid2taxid(filename, columns=None):
     readid2taxid = {}
     with open(filename, 'r') as f:
         for line in f.readlines():
             line = line.strip().split('\t')
-            readid2taxid[line[0]] = int(line[1])
+            if columns is None:
+                readid2taxid[line[0]] = int(line[1])
+            else:
+                readid2taxid[line[columns[0]]] = int(line[columns[1]])
     return readid2taxid
 
-def get_krakenuniq_readid2readid(filename):
-    krakenuniq_accession2taxid = get_readid2taxid(filename)
-    accession2taxid = get_readid2taxid(filename + ".orig")
+
+def get_krakenuniq_taxid2taxid(filename):
+    krakenuniq_seq2taxid = get_seqid2taxid(filename)
+    seqid2taxid = get_seqid2taxid(filename + ".orig")
     taxid2taxid = {}
-    for accession, taxid in krakenuniq_accession2taxid.items():
-        real_taxid = accession2taxid[accession]
+    for seqid, taxid in krakenuniq_seq2taxid.items():
+        real_taxid = seqid2taxid[seqid]
+        taxid2taxid[taxid] = real_taxid
+    return taxid2taxid
+
+
+def get_kasa_taxid2taxid(ref_content, seqid2taxid):
+    kasa_readid2taxid = get_seqid2taxid(ref_content, (3, 2))
+    seqid2taxid = get_seqid2taxid(seqid2taxid)
+    taxid2taxid = {}
+    for seqid, taxid in kasa_readid2taxid.items():
+        real_taxid = seqid2taxid[seqid]
         taxid2taxid[taxid] = real_taxid
     return taxid2taxid
 
@@ -37,9 +51,20 @@ def main():
         dest="krakenuniq_map",
         default=None,
         help="The seqid2taxid.map file provided by krakenuniq and the accession2taxid mapping for the dataset")
+    parser.add_argument(
+        "-k",
+        "--kasa",
+        dest="kasa_map_and_seqid2taxid",
+        default=None,
+        help="<ref_content.txt>,<seqid2taxid> -- ref_content.txt provided by kASA's database")
     parser.add_argument("file", help="read id to tax id file to reformat")
     args = parser.parse_args()
-    krakenuniq_taxid2taxid = None if args.krakenuniq_map is None else get_krakenuniq_readid2readid(args.krakenuniq_map)
+    krakenuniq_taxid2taxid = None if args.krakenuniq_map is None else get_krakenuniq_taxid2taxid(args.krakenuniq_map)
+    kasa_files = None
+    if args.kasa_map_and_seqid2taxid is not None:
+        split_files = list(args.kasa_map_and_seqid2taxid.split(","))
+        kasa_files = (split_files[0], split_files[1])
+    kasa_taxid2taxid = None if args.kasa_map_and_seqid2taxid is None else get_kasa_taxid2taxid(kasa_files[0], kasa_files[1])
 
     # Initialize event logger
     logging.basicConfig(
@@ -63,6 +88,9 @@ def main():
                 if value > 1000000000:
                     value = krakenuniq_taxid2taxid[value]
                 value = str(value)
+            elif kasa_taxid2taxid is not None:
+                value = int(value)
+                value = str(kasa_taxid2taxid[value])
             print(f"{readid}\t{value}")
 
     logging.info("Done reformatting!")
