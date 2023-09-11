d7abe7a5002c424c7e8e98ba5287bd69532b0d6c
trevor <trevor.schneggenburger@gmail.com>
2023-09-11 10:27:03 -0400

    Aggregated read length stats instead of printing out each

--- misc/print-read-lengths.py
+++ misc/print-read-lengths.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import sys
-from functools import reduce
 
 from Bio import SeqIO
 from taxonomy.taxonomy import Taxonomy
@@ -10,9 +9,9 @@ from taxonomy.taxonomy import Taxonomy
 def main():
     # Parse arguments from command line
     parser = argparse.ArgumentParser(
-        description="Takes a fastq reads file and prints all the read lengths line by line")
+        description="Takes a fasta reads file and prints all the read lengths line by line")
     parser.add_argument(
-        "fastq_file",
+        "fasta_file",
         help="The reads file")
     args = parser.parse_args()
 
@@ -23,9 +22,24 @@ def main():
         format='[%(asctime)s %(threadName)s %(levelname)s] %(message)s',
         datefmt='%m-%d-%Y %I:%M:%S%p')
 
+    length2count = {}
+    longest_length = 0
     logging.info(f"Printing all sequence lengths from {args.fastq_file}")
-    for record in SeqIO.parse(args.fastq_file, "fastq"):
-        print(len(record.seq))
+    for record in SeqIO.parse(args.fastq_file, "fasta"):
+        length = len(record.seq)
+        if length > longest_length:
+            longest_length = length
+        if length in length2count:
+            length2count[length] += 1
+        else:
+            length2count[length] = 1
+
+    for i in range(longest_length + 1):
+        if i in length2count:
+            print(f"{str(i)}\t{str(length2count[i])}")
+        else:
+            print(f"{str(i)}\t0")
+
     logging.info("Done!")
 
 
