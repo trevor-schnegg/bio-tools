c3d0118201b16e473df08c126bbfd688a00786f9
Trevor <trevor.schneggenburger@gmail.com>
2024-09-02 16:23:50 -0400

    Code cleaning and added comments

--- misc/clark-hack.py
+++ misc/clark-hack.py
@@ -3,22 +3,28 @@ import logging
 import sys
 
 
-def get_accession2taxid(file):
+def load_accession2taxid(file):
     accession2taxid = {}
-    with open(file, 'r') as accession2taxid_file:
-        for line in accession2taxid_file:
+    with open(file, 'r') as f:
+        for line in f:
             line = line.strip()
             if line.__contains__("\t"):
+                # If the line has a tab, split on tab
                 split_line = line.split("\t")
             else:
+                # Otherwise, split on space
                 split_line = line.split(" ")
+
+            # If the accession has a period, remove everything after the period
             accession = split_line[0].split(".")[0]
+
+            # Check to make sure the same accession isn't assigned a tax id twice
             if accession in accession2taxid and accession2taxid[accession] != split_line[-1]:
                 logging.error(
                     f"{accession} appeared twice with taxids {accession2taxid[accession]} and {split_line[-1]}")
                 logging.error(
-                    f"please fix this before running again - aborting")
-                sys.exit()
+                    f"please fix this before running again - exiting")
+                sys.exit(1)
             else:
                 accession2taxid[accession] = split_line[-1]
     return accession2taxid
@@ -26,7 +32,7 @@ def get_accession2taxid(file):
 def main():
     # Parse arguments from command line
     parser = argparse.ArgumentParser(
-        description="Takes the .custom.fileToAccssnTaxID file from CLARK and updates the tax ids according to a accession2taxid")
+        description="Takes the .custom.fileToAccssnTaxID file from CLARK and updates the tax ids according to a accession2taxid (prints to stdout)")
     parser.add_argument(
         "accession2taxid",
         help="accession2taxid of reference file")
@@ -45,7 +51,7 @@ def main():
     # Read in accession2taxid
     logging.info(
         f"Reading accession2taxid at {args.accession2taxid}")
-    accession2taxid = get_accession2taxid(args.accession2taxid)
+    accession2taxid = load_accession2taxid(args.accession2taxid)
     logging.info("Done reading accession2taxid!")
 
     with open(args.file_to_accession_taxid, 'r') as f:
