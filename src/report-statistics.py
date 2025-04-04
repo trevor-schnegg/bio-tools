8dc2ffe9a92e2ee3582eb6978b02673c6e9137ea
Trevor <trevor.schneggenburger@gmail.com>
2025-04-04 11:37:20 -0400

    Reformatted report-statistics.py and added try statements to catch division by 0

--- src/report-statistics.py
+++ src/report-statistics.py
@@ -7,73 +7,80 @@ from taxonomy.taxonomy import Taxonomy
 
 def get_readid2taxid(filename):
     readid2taxid = {}
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
-            line = line.strip().split('\t')
+            line = line.strip().split("\t")
             readid2taxid[line[0]] = int(line[1])
     return readid2taxid
 
+
 def get_taxids_in_reference(filename, taxonomy, evaluation_levels):
     taxids = set()
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
-            line = line.strip().split('\t')
+            line = line.strip().split("\t")
             for level in evaluation_levels:
                 taxid = taxonomy.parent(line[1], at_rank=level)
                 if taxid is not None:
                     taxids.add(taxid.id)
     return taxids
 
+
 def main():
     # Parse arguments from command line
     parser = argparse.ArgumentParser(
         description="Takes a ground truth read id to tax id mapping and computes precision, recall, accuracy, etc. "
-                    "of a classifier")
+        "of a classifier"
+    )
     parser.add_argument(
         "-g",
         "--give-formulas",
         dest="give_formulas",
         action="store_true",
-        help="If specified, print the formulas used for precision, recall, and accuracy"
+        help="If specified, print the formulas used for precision, recall, and accuracy",
     )
     parser.add_argument(
         "-i",
         "--include-header",
         dest="include_header",
         action="store_true",
-        help="Prints the header line before the output")
+        help="Prints the header line before the output",
+    )
     parser.add_argument(
         "-u",
         "--ignore-unclassified",
         dest="ignore_unclassified",
         action="store_true",
-        help="Include this option if you want to exclude reads unclassified by the ground truth")
+        help="Include this option if you want to exclude reads unclassified by the ground truth",
+    )
     parser.add_argument(
         "-c",
         "--classifier-name",
         dest="classifier_name",
         default=None,
-        help="The name of the classifier whose accuracy is being evaluated")
-    parser.add_argument(
-        "taxonomy",
-        help="NCBI taxonomy directory")
+        help="The name of the classifier whose accuracy is being evaluated",
+    )
+    parser.add_argument("taxonomy", help="NCBI taxonomy directory")
     parser.add_argument(
         "ground_truth_readid2taxid",
-        help="Tab separated read id to tax id of bwa-mem or other ground truth")
+        help="Tab separated read id to tax id of bwa-mem or other ground truth",
+    )
     parser.add_argument(
         "predicted_readid2taxid",
-        help="Tab separated read id to tax id of the classifier")
+        help="Tab separated read id to tax id of the classifier",
+    )
     parser.add_argument(
-        "reference_seqid2taxid",
-        help="Tab separated seqid to taxid of the reference")
+        "reference_seqid2taxid", help="Tab separated seqid to taxid of the reference"
+    )
     args = parser.parse_args()
 
     # Initialize event logger
     logging.basicConfig(
         stream=sys.stderr,
         level=logging.DEBUG,
-        format='[%(asctime)s %(threadName)s %(levelname)s] %(message)s',
-        datefmt='%m-%d-%Y %I:%M:%S%p')
+        format="[%(asctime)s %(threadName)s %(levelname)s] %(message)s",
+        datefmt="%m-%d-%Y %I:%M:%S%p",
+    )
 
     evaluation_levels = ["genus", "species"]
 
@@ -82,21 +89,24 @@ def main():
     taxonomy: Taxonomy = Taxonomy.from_ncbi(args.taxonomy)
 
     logging.info(
-        f"Reading ground truth readid2taxid from {args.ground_truth_readid2taxid}")
+        f"Reading ground truth readid2taxid from {args.ground_truth_readid2taxid}"
+    )
     # Read both readid2taxids
-    ground_truth_readid2taxid = get_readid2taxid(
-        args.ground_truth_readid2taxid)
-    logging.info(
-        f"Reading predicted readid2taxid from {args.predicted_readid2taxid}")
+    ground_truth_readid2taxid = get_readid2taxid(args.ground_truth_readid2taxid)
+    logging.info(f"Reading predicted readid2taxid from {args.predicted_readid2taxid}")
     predicted_readid2taxid = get_readid2taxid(args.predicted_readid2taxid)
     logging.info("Both readid2taxids read!")
 
     logging.info("Reading taxids from the reference")
-    reference_taxids = get_taxids_in_reference(args.reference_seqid2taxid, taxonomy, evaluation_levels)
+    reference_taxids = get_taxids_in_reference(
+        args.reference_seqid2taxid, taxonomy, evaluation_levels
+    )
     logging.info("Reference taxids obtained!")
 
     # Compute the desired statistics for each tax id
-    logging.info(f"Computing statistics for {args.classifier_name}, this takes about 15 seconds max...")
+    logging.info(
+        f"Computing statistics for {args.classifier_name}, this takes about 15 seconds max..."
+    )
     stats = {}
     for level in evaluation_levels:
         stats[level + "_total"] = 0
@@ -120,14 +130,13 @@ def main():
             true_lineage = cache[true_taxid]
         else:
             for tax_level in evaluation_levels:
-                true_lineage.append(
-                    taxonomy.parent(
-                        str(true_taxid),
-                        at_rank=tax_level))
+                true_lineage.append(taxonomy.parent(str(true_taxid), at_rank=tax_level))
             cache[true_taxid] = true_lineage
 
         # Get the predicted taxid
-        predicted_taxid = predicted_readid2taxid[readid] if readid in predicted_readid2taxid else 0
+        predicted_taxid = (
+            predicted_readid2taxid[readid] if readid in predicted_readid2taxid else 0
+        )
 
         # Get the lineage nodes for the predicted taxid
         predicted_lineage = []
@@ -136,14 +145,14 @@ def main():
         else:
             for tax_level in evaluation_levels:
                 predicted_lineage.append(
-                    taxonomy.parent(
-                        str(predicted_taxid),
-                        at_rank=tax_level))
+                    taxonomy.parent(str(predicted_taxid), at_rank=tax_level)
+                )
             cache[predicted_taxid] = predicted_lineage
 
         # Increment the correct count based on the observed lineage
         for true_node, predicted_node, level in zip(
-                true_lineage, predicted_lineage, evaluation_levels):
+            true_lineage, predicted_lineage, evaluation_levels
+        ):
 
             if true_node is None and predicted_node is None:
                 stats[level + "_unclassified_tn"] += 1
@@ -182,8 +191,12 @@ def main():
 
         if taxonomy_issue_detected:
             # Log issue if there was one
-            logging.warning("Argument to ignore unclassified reads provided, but None values were detected in the ground truth")
-            logging.warning("There is likely an issue with the taxonomy or the ground truth read assignments")
+            logging.warning(
+                "Argument to ignore unclassified reads provided, but None values were detected in the ground truth"
+            )
+            logging.warning(
+                "There is likely an issue with the taxonomy or the ground truth read assignments"
+            )
 
     # Print formulas if needed
     if args.give_formulas:
@@ -200,23 +213,60 @@ def main():
         print_string += "<No name provided>\t"
 
     genus_tp, genus_fn = stats["genus_tp"], stats["genus_fn"]
-    genus_fp = stats["genus_fp"] + stats["genus_unclassified_fp"] + stats["genus_not_in_ref_fp"]
+    genus_fp = (
+        stats["genus_fp"]
+        + stats["genus_unclassified_fp"]
+        + stats["genus_not_in_ref_fp"]
+    )
     genus_tn = stats["genus_unclassified_tn"] + stats["genus_not_in_ref_tn"]
     species_tp, species_fn = stats["species_tp"], stats["species_fn"]
-    species_fp = stats["species_fp"] + stats["species_unclassified_fp"] + stats["species_not_in_ref_fp"]
+    species_fp = (
+        stats["species_fp"]
+        + stats["species_unclassified_fp"]
+        + stats["species_not_in_ref_fp"]
+    )
     species_tn = stats["species_unclassified_tn"] + stats["species_not_in_ref_tn"]
 
-    genus_recall = (genus_tp/(genus_tp+genus_fn)) * 100
-    genus_precision = (genus_tp/(genus_tp+genus_fp)) * 100
-    genus_accuracy = ((genus_tp+genus_tn)/(genus_tp+genus_tn+genus_fn+genus_fp)) * 100
+    try:
+        genus_recall = (genus_tp / (genus_tp + genus_fn)) * 100
+    except ZeroDivisionError:
+        genus_recall = "undef"
 
-    species_recall = (species_tp/(species_tp+species_fn)) * 100
-    species_precision = (species_tp/(species_tp+species_fp)) * 100
-    species_accuracy = ((species_tp+species_tn)/(species_tp+species_tn+species_fn+species_fp)) * 100
+    try:
+        genus_precision = (genus_tp / (genus_tp + genus_fp)) * 100
+    except ZeroDivisionError:
+        genus_precision = "undef"
+
+    try:
+        genus_accuracy = (
+            (genus_tp + genus_tn) / (genus_tp + genus_tn + genus_fn + genus_fp)
+        ) * 100
+    except ZeroDivisionError:
+        genus_accuracy = "undef"
+
+    try:
+        species_recall = (species_tp / (species_tp + species_fn)) * 100
+    except ZeroDivisionError:
+        species_recall = "undef"
+
+    try:
+        species_precision = (species_tp / (species_tp + species_fp)) * 100
+    except ZeroDivisionError:
+        species_precision = "undef"
+
+    try:
+        species_accuracy = (
+            (species_tp + species_tn)
+            / (species_tp + species_tn + species_fn + species_fp)
+        ) * 100
+    except ZeroDivisionError:
+        species_accuracy = "undef"
 
     if args.include_header:
-        print("classifier\tgenus_recall\tgenus_precision\tgenus_accuracy\tspecies_recall\tspecies_precision\tspecies_accuracy\t"
-              "genus_TP\tgenus_FP\tgenus_FN\tgenus_TN\tspecies_TP\tspecies_FP\tspecies_FN\tspecies_TN")
+        print(
+            "classifier\tgenus_recall\tgenus_precision\tgenus_accuracy\tspecies_recall\tspecies_precision\tspecies_accuracy\t"
+            "genus_TP\tgenus_FP\tgenus_FN\tgenus_TN\tspecies_TP\tspecies_FP\tspecies_FN\tspecies_TN"
+        )
 
     print_string += f"{genus_recall}\t{genus_precision}\t{genus_accuracy}\t{species_recall}\t{species_precision}\t{species_accuracy}\t"
     print_string += f"{genus_tp}\t{genus_fp}\t{genus_fn}\t{genus_tn}\t{species_tp}\t{species_fp}\t{species_fn}\t{species_tn}"
@@ -225,5 +275,5 @@ def main():
     logging.info("Done reporting statistics!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
